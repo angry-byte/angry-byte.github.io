27356eb223e6a8f3ca624c92dfbeabb6a935435a
angry-byte <contact@angry-byte.com>
2022-02-19 15:40:26 +0300

    Spelling correction on post11

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220219.2006';
+const cacheName = 'chirpy-20220219.2039';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
