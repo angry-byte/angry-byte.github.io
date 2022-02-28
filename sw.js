d14c36a85374acc9ad18abfb19d1bc5c742c4649
angry-byte <contact@angry-byte.com>
2022-02-28 10:02:03 +0300

    Yet another minor update to post12

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220228.1458';
+const cacheName = 'chirpy-20220228.1501';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
