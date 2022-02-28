17ff701a3b574a1efc6e601c5257bca8b0a1ceb4
angry-byte <contact@angry-byte.com>
2022-02-28 09:55:11 +0300

    Upload post 12 and minor update to about.md

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220219.2039';
+const cacheName = 'chirpy-20220228.1453';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
